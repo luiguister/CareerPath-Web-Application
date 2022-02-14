b6609135ce91566852a4704e2155e8671788d9e2
luiguister <luigui790@hotmail.com>
2022-02-14 11:59:13 -0500

    1.9

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ router.get("/", function(req,res){
 router.get("/details", function(req,resa){
     var cpath = req.query.cpath;
     var options = {
-        host: 'http://careerpathwebservice.herokuapp.com/',
+        host: http + '://careerpathwebservice.herokuapp.com/',
         port: 8080,
         path: '/cources/' + cpath,
         method: 'GET'
